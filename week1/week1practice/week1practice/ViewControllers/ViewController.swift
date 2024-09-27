dc1d902fc00c11827650f9c6fc3b1cdce04ca7c6
MijuKim <alwn8918@gmail.com>
2024-09-28 01:45:49 +0900

    ✨ [week1] 숫자 증가/감소 기능 구현

--- week1/week1practice/week1practice/ViewControllers/ViewController.swift
+++ week1/week1practice/week1practice/ViewControllers/ViewController.swift
@@ -19,8 +19,24 @@ class ViewController: UIViewController {
     private lazy var counterView: CountUpDownView = {
         let view = CountUpDownView()
         view.countLabel.text = String(data.count)
+        view.countDownButton.addTarget(self, action: #selector(decreaseCount), for: .touchUpInside)
+        view.countUpButton.addTarget(self, action: #selector(increaseCount), for: .touchUpInside)
         return view
     }()
+    
+    @objc func increaseCount() {
+        self.data.increaseCount()
+        self.updateCount()
+    }
+    
+    @objc func decreaseCount() {
+        self.data.decreaseCount()
+        self.updateCount()
+    }
+    
+    private func updateCount() {
+        self.counterView.countLabel.text = String(data.count)
+    }
 
 }
 
