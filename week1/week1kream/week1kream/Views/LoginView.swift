2556bdf851fb90ddfc5ff6bcaddce1a2ca5b7a5a
MijuKim <alwn8918@gmail.com>
2024-09-28 03:43:16 +0900

    💄 [week1] 이메일 textfield 생성 및 배치

--- week1/week1kream/week1kream/Views/LoginView.swift
+++ week1/week1kream/week1kream/Views/LoginView.swift
@@ -18,6 +18,7 @@ class LoginView: UIView {
     private func addComponents() {
         self.addSubview(logoImageView)
         self.addSubview(emailLabel)
+        self.addSubview(emailTextField)
         
         NSLayoutConstraint.activate([
             logoImageView.topAnchor.constraint(equalTo: self.topAnchor, constant: 126),
@@ -26,7 +27,12 @@ class LoginView: UIView {
             
             emailLabel.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: 87),
             emailLabel.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 45),
-            emailLabel.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -45)
+            emailLabel.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -45),
+            
+            emailTextField.topAnchor.constraint(equalTo: emailLabel.bottomAnchor, constant: 8),
+            emailTextField.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 45),
+            emailTextField.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -45),
+            emailTextField.heightAnchor.constraint(equalToConstant: 34)
         ])
     }
     
@@ -55,5 +61,30 @@ class LoginView: UIView {
         
         return label
     }()
+    
+    public lazy var emailTextField: UITextField = {
+        let textfield = UITextField()
+        
+        textfield.addLeftPadding()
+        textfield.placeholder = "예) kream@kream.co.kr"
+        textfield.font = UIFont.systemFont(ofSize: 12)
+        
+        textfield.layer.borderWidth = 1
+        textfield.layer.borderColor = UIColor(red: 162/255, green: 162/255, blue: 162/255, alpha: 1).cgColor
+        textfield.layer.cornerRadius = CGFloat(15)
+        
+        textfield.translatesAutoresizingMaskIntoConstraints = false
+        
+        return textfield
+    }()
 
+}
+
+// textfield 왼쪽 여백
+extension UITextField {
+    func addLeftPadding() {
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: 16, height: self.frame.height))
+        self.leftView = paddingView
+        self.leftViewMode = ViewMode.always
+    }
 }
