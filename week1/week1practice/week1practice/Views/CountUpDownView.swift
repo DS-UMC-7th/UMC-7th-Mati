9a5d4e1401a16233342a949278604acf650ba909
MijuKim <alwn8918@gmail.com>
2024-09-28 01:35:31 +0900

    💄 [week1] label, button 생성

--- week1/week1practice/week1practice/Views/CountUpDownView.swift
+++ week1/week1practice/week1practice/Views/CountUpDownView.swift
@@ -20,4 +20,61 @@ class CountUpDownView: UIView {
         fatalError("Init(coder:) has not been implemented")
     }
     
+    // 카운트 표시 라벨
+    public lazy var countLabel: UILabel = {
+        let label = UILabel()
+        
+        label.font = UIFont.systemFont(ofSize: 42, weight: .bold)
+        label.textColor = UIColor.black
+        label.textAlignment = .center
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        return label
+    }()
+    
+    // 버튼 내부 텍스트 폰트 지정 컨테이너
+    private lazy var titleContainer: AttributeContainer = {
+        var container = AttributeContainer()
+        container.font = UIFont.systemFont(ofSize: 16, weight: .bold)
+        container.foregroundColor = UIColor.black
+        return container
+    }()
+    
+    // 숫자 업 버튼
+    public lazy var countUpButton: UIButton = {
+        let btn = UIButton()
+        
+        var configuration = UIButton.Configuration.plain()
+        configuration.image = UIImage(systemName: "arrowtriangle.up.fill")?.withRenderingMode(.alwaysOriginal).withTintColor(.black)
+        configuration.imagePlacement = .top
+        configuration.imagePadding = 5
+        
+        configuration.attributedTitle = AttributedString("숫자 올리기", attributes: titleContainer)
+        configuration.titleAlignment = .center
+        
+        btn.configuration = configuration
+        btn.translatesAutoresizingMaskIntoConstraints = false
+        
+        return btn
+    }()
+    
+    // 숫자 다운 버튼
+    public lazy var countDownButton: UIButton = {
+        let btn = UIButton()
+        
+        var configuration = UIButton.Configuration.plain()
+        configuration.image = UIImage(systemName: "arrowtriangle.down.fill")?.withRenderingMode(.alwaysOriginal).withTintColor(.black)
+        configuration.imagePlacement = .top
+        configuration.imagePadding = 5
+        
+        configuration.attributedTitle = AttributedString("숫자 내리기", attributes: titleContainer)
+        configuration.titleAlignment = .center
+        
+        btn.configuration = configuration
+        btn.translatesAutoresizingMaskIntoConstraints = false
+        
+        return btn
+    }()
+    
 }
