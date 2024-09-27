cdff1cb81069ea41d5a48d0751106b0a864ae920
MijuKim <alwn8918@gmail.com>
2024-09-28 01:40:36 +0900

    ✨ [week1] 모델 연결

--- week1/week1practice/week1practice/ViewControllers/ViewController.swift
+++ week1/week1practice/week1practice/ViewControllers/ViewController.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var data: CountUpDownModel = CountUpDownModel(count: 1)
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,7 +18,7 @@ class ViewController: UIViewController {
     
     private lazy var counterView: CountUpDownView = {
         let view = CountUpDownView()
-        view.countLabel.text = "1"
+        view.countLabel.text = String(data.count)
         return view
     }()
 
