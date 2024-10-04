d1f0e1176999b1ec09ef44f22a14e927ace16f44
MijuKim <alwn8918@gmail.com>
2024-10-05 05:12:56 +0900

    💄 [week2] SnapKit으로 AutoLayout 구현하기

--- week2/week2practice/week2practice/ModalViewController.swift
+++ week2/week2practice/week2practice/ModalViewController.swift
@@ -6,24 +6,33 @@
 //
 
 import UIKit
+import SnapKit
 
 class ModalViewController: UIViewController {
+    private let label = UILabel()
+    private let button = UIButton()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        label.text = "버튼을 누르면 모달 방식으로 화면 전환이 일어나요"
+        label.textColor = .cyan
+        
+        button.setTitle("버튼을 눌러주세요", for: .normal)
+        button.backgroundColor = .systemIndigo
 
-        // Do any additional setup after loading the view.
+        view.addSubview(label)
+        view.addSubview(button)
+        
+        label.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(80)
+            $0.centerX.equalToSuperview()
+        }
+        
+        button.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(100)
+            $0.centerX.equalToSuperview()
+        }
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
