26b8e1400aed85960eccd3c18e44e22d1468abcc
MijuKim <alwn8918@gmail.com>
2024-10-05 04:46:07 +0900

    ✨ [week2] TabBarController 구현하기

---
+++ week2/week2practice/week2practice/ModalViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ModalViewController.swift
+//  week2practice
+//
+//  Created by 김미주 on 05/10/2024.
+//
+
+import UIKit
+
+class ModalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
