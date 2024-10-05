86b07528f6d9736123c51fbf1132c51a794ff6c5
MijuKim <alwn8918@gmail.com>
2024-10-06 01:48:21 +0900

    💄 [week2] kream 마이페이지 레이아웃 구현

---
+++ week2/week2kream/week2kream/ViewControllers/MainViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MainViewController.swift
+//  week2kream
+//
+//  Created by 김미주 on 05/10/2024.
+//
+
+import UIKit
+
+class MainViewController: UITabBarController {
+    private let homeVC = HomeViewController()
+    private let styleVC = StyleViewController()
+    private let shopVC = ShopViewController()
+    private let savedVC = SavedViewController()
+    private let myVC = MyViewController()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        
+        homeVC.tabBarItem = UITabBarItem(title: "HOME", image: UIImage(named: "home_icon"), tag: 0)
+        styleVC.tabBarItem = UITabBarItem(title: "STYLE", image: UIImage(named: "style_icon"), tag: 1)
+        shopVC.tabBarItem = UITabBarItem(title: "SHOP", image: UIImage(named: "shop_icon"), tag: 2)
+        savedVC.tabBarItem = UITabBarItem(title: "SAVED", image: UIImage(named: "saved_icon"), tag: 3)
+        myVC.tabBarItem = UITabBarItem(title: "MY", image: UIImage(named: "my_icon"), tag: 4)
+
+        self.viewControllers = [homeVC, styleVC, shopVC, savedVC, myVC]
+    }
+
+}
---
+++ week2/week2kream/week2kream/ViewControllers/MyViewController.swift
@@ -0,0 +1,22 @@
+//
+//  MyViewController.swift
+//  week2kream
+//
+//  Created by 김미주 on 05/10/2024.
+//
+
+import UIKit
+
+class MyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view = myView
+    }
+    
+    private lazy var myView: MyView = {
+        let view = MyView()
+        return view
+    }()
+
+}
