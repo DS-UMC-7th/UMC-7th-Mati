5ce0af41882198ab2fee314d4591fe4830cdf0b2
alwn8918 <alwn8918@gmail.com>
2024-10-08 14:48:14 +0900

    ✨ [week3] kream 앱 생성

---
+++ kream/kream/ViewControllers/StyleViewController.swift
@@ -0,0 +1,29 @@
+//
+//  StyleViewController.swift
+//  week2kream
+//
+//  Created by 김미주 on 05/10/2024.
+//
+
+import UIKit
+
+class StyleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
