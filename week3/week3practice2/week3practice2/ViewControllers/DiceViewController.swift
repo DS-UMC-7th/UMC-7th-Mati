35fab882bbe5aaf343e26738fb053ad069629448
alwn8918 <alwn8918@gmail.com>
2024-10-12 02:17:58 +0900

    ✨ [week3] 실습: ViewController 작성

--- week3/week3practice2/week3practice2/ViewControllers/DiceViewController.swift
+++ week3/week3practice2/week3practice2/ViewControllers/DiceViewController.swift
@@ -8,22 +8,37 @@
 import UIKit
 
 class DiceViewController: UIViewController {
+    
+    let data = dummyDiceModel.diceDatas
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.view = diceView
     }
     
+    private lazy var diceView: DiceView = {
+        let view = DiceView()
+        
+        view.tableView.dataSource = self
+        view.tableView.delegate = self
+        
+        return view
+    }()
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+// MARK: - dataSource, delegate 구현
+extension DiceViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        data.count
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: DiceCell.identifier, for: indexPath) as? DiceCell else {
+            return UITableViewCell()
+        }
+        
+        cell.configure(model: data[indexPath.row])
+        
+        return cell
+    }
 }
