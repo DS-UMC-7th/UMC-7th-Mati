919a630cb7cac40b59f32c806977be14be97ede4
alwn8918 <alwn8918@gmail.com>
2024-10-15 22:22:16 +0900

    🎉 [week4] 실습: UISegmentedControl 실습 프로젝트 생성

---
+++ week4/week4UISegmentedControl/week4UISegmentedControl/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  week4UISegmentedControl
+//
+//  Created by 김미주 on 15/10/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
