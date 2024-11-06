49b5ea7271ceaa2a8d4e3674a056f2fad9739e08
alwn8918 <alwn8918@gmail.com>
2024-11-06 20:02:25 +0900

    🐛 [week4] kream: collectionView 스크롤 가능하도록 수정

--- kream/kream/Views/HomeView.swift
+++ kream/kream/Views/HomeView.swift
@@ -54,7 +54,6 @@ class HomeView: UIView {
         $0.minimumLineSpacing = 9
     }).then {
         $0.backgroundColor = .clear
-        $0.isScrollEnabled = false
         $0.register(HomeCollectionViewCell.self, forCellWithReuseIdentifier: HomeCollectionViewCell.identifier)
     }
     
