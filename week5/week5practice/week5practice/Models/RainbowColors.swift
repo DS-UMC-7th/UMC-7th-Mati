80a5554be674380246f1cd233063357e9386785a
alwn8918 <alwn8918@gmail.com>
2024-11-06 23:13:11 +0900

    ✨ [week5] 실습: Model 작성

--- week5/week5practice/week5practice/Models/RainbowColors.swift
+++ week5/week5practice/week5practice/Models/RainbowColors.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+import UIKit
+
+struct RainbowColors {
+    static let colors: [UIColor] = [
+        UIColor.red,
+        UIColor.orange,
+        UIColor.yellow,
+        UIColor.green,
+        UIColor.blue,
+        UIColor(red: 0.294, green: 0, blue: 0.51, alpha: 1),
+        UIColor.purple
+    ]
+}
