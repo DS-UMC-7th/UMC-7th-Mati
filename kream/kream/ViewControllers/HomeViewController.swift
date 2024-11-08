baa11d568ea262df5b1bb58c865c06540876edd1
alwn8918 <alwn8918@gmail.com>
2024-11-09 02:57:22 +0900

    🐛 [week6] kream: 네비게이션 상단 바 제거

--- kream/kream/ViewControllers/HomeViewController.swift
+++ kream/kream/ViewControllers/HomeViewController.swift
@@ -12,6 +12,7 @@ class HomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.navigationController?.isNavigationBarHidden = true
 
         view = homeView
         
