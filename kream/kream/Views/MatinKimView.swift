43deb5e3b27c2a96d656255bf5efe31dfcb8a9db
alwn8918 <alwn8918@gmail.com>
2024-11-09 03:17:42 +0900

    💄 [week6] kream: 상품 이미지, 가격, 이름 추가

--- kream/kream/Views/MatinKimView.swift
+++ kream/kream/Views/MatinKimView.swift
@@ -15,12 +15,37 @@ class MatinKimView: UIView {
     }
     
     // 이미지
+    let imageView = UIImageView().then {
+        $0.image = UIImage(resource: .imageMatinkim)
+    }
     
     // 옵션
     
     // 가격
+    let priceTitle = UILabel().then {
+        $0.text = "즉시 구매가"
+        $0.font = .systemFont(ofSize: 12, weight: .light)
+        $0.textColor = .black
+    }
+    
+    let priceLabel = UILabel().then {
+        $0.text = "228,000원"
+        $0.font = .systemFont(ofSize: 20, weight: .semibold)
+        $0.textColor = .black
+    }
     
     // 이름
+    let nameEng = UILabel().then {
+        $0.text = "Matin Kim Logo Coating Jumper"
+        $0.font = .systemFont(ofSize: 16, weight: .regular)
+        $0.textColor = .black
+    }
+    
+    let nameKor = UILabel().then {
+        $0.text = "마뗑킴 로고 코팅 점퍼 블랙"
+        $0.font = .systemFont(ofSize: 12, weight: .regular)
+        $0.textColor = UIColor(red: 156/255, green: 156/255, blue: 156/255, alpha: 1)
+    }
     
     // 구분선
     
@@ -45,7 +70,12 @@ class MatinKimView: UIView {
     // MARK: - func
     private func setupView() {
         [
-            backButton
+            backButton,
+            imageView,
+            priceTitle,
+            priceLabel,
+            nameEng,
+            nameKor,
         ].forEach {
             addSubview($0)
         }
@@ -54,6 +84,31 @@ class MatinKimView: UIView {
             $0.top.equalTo(safeAreaLayoutGuide).offset(14)
             $0.left.equalToSuperview().offset(15)
             $0.width.height.equalTo(24)
+        }
+        
+        imageView.snp.makeConstraints {
+            $0.top.equalTo(backButton.snp.bottom).offset(10)
+            $0.horizontalEdges.equalToSuperview()
+        }
+        
+        priceTitle.snp.makeConstraints {
+            $0.top.equalTo(imageView.snp.bottom).offset(23)
+            $0.left.equalToSuperview().offset(16)
+        }
+        
+        priceLabel.snp.makeConstraints {
+            $0.top.equalTo(priceTitle.snp.bottom).offset(4)
+            $0.left.equalToSuperview().offset(16)
+        }
+        
+        nameEng.snp.makeConstraints {
+            $0.top.equalTo(priceLabel.snp.bottom).offset(18)
+            $0.left.equalToSuperview().offset(16)
+        }
+        
+        nameKor.snp.makeConstraints {
+            $0.top.equalTo(nameEng.snp.bottom).offset(6)
+            $0.left.equalToSuperview().offset(16)
         }
     }
 }
