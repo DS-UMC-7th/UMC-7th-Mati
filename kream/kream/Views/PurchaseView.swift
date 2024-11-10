042e639636790fac818ec3bf27b16d5d7a51870f
alwn8918 <alwn8918@gmail.com>
2024-11-10 23:07:01 +0900

    💄 [week6] kream: 구매탭 사이즈 버튼 제외 디자인 구현

--- kream/kream/Views/PurchaseView.swift
+++ kream/kream/Views/PurchaseView.swift
@@ -8,14 +8,105 @@
 import UIKit
 
 class PurchaseView: UIView {
-
+    // MARK: - view
+    // 타이틀
+    let titleLabel = UILabel().then {
+        $0.text = "구매하기"
+        $0.font = .systemFont(ofSize: 15, weight: .bold)
+        $0.textColor = .black
+        $0.textAlignment = .center
+    }
+    
+    let subTitleLabel = UILabel().then {
+        $0.text = "(가격 단위: 원)"
+        $0.font = .systemFont(ofSize: 10, weight: .regular)
+        $0.textColor = UIColor(red: 146/255, green: 146/255, blue: 146/255, alpha: 1)
+        $0.textAlignment = .center
+    }
+    
+    // x 버튼
+    let cancelButton = UIButton().then {
+        $0.setImage(.iconCancel, for: .normal)
+    }
+    
+    // 상품 이미지
+    let imageView = UIImageView().then {
+        $0.image = .imageMatinkim
+        $0.layer.cornerRadius = 8
+        $0.clipsToBounds = true
+    }
+    
+    // 상품 이름
+    let nameEng = UILabel().then {
+        $0.text = "Matin Kim Logo Coating Jumper"
+        $0.font = .systemFont(ofSize: 14, weight: .regular)
+        $0.textColor = .black
+    }
+    
+    let nameKor = UILabel().then {
+        $0.text = "마뗑킴 로고 코팅 점퍼 블랙"
+        $0.font = .systemFont(ofSize: 12, weight: .regular)
+        $0.textColor = UIColor(red: 156/255, green: 156/255, blue: 156/255, alpha: 1)
+    }
+    
+    // 사이즈 - 컬렉션 뷰
+    
+    // MARK: - init
     override init(frame: CGRect) {
         super.init(frame: frame)
         self.backgroundColor = .white
+        
+        setupView()
     }
     
     required init?(coder: NSCoder) {
         fatalError()
+    }
+    
+    // MARK: - function
+    private func setupView() {
+        [
+            titleLabel,
+            subTitleLabel,
+            cancelButton,
+            imageView,
+            nameEng,
+            nameKor
+        ].forEach {
+            addSubview($0)
+        }
+        
+        titleLabel.snp.makeConstraints {
+            $0.top.equalTo(safeAreaLayoutGuide).offset(20)
+            $0.horizontalEdges.equalToSuperview().inset(160)
+        }
+        
+        subTitleLabel.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom)
+            $0.horizontalEdges.equalToSuperview().inset(158)
+        }
+        
+        cancelButton.snp.makeConstraints {
+            $0.top.equalTo(safeAreaLayoutGuide).offset(20)
+            $0.right.equalToSuperview().offset(-16)
+            $0.width.height.equalTo(20)
+        }
+        
+        imageView.snp.makeConstraints {
+            $0.top.equalTo(subTitleLabel.snp.bottom).offset(11)
+            $0.left.equalToSuperview().offset(16)
+            $0.width.height.equalTo(91)
+        }
+        
+        nameEng.snp.makeConstraints {
+            $0.top.equalTo(imageView.snp.top).offset(18)
+            $0.left.equalTo(imageView.snp.right).offset(15)
+        }
+        
+        nameKor.snp.makeConstraints {
+            $0.top.equalTo(nameEng.snp.bottom).offset(4)
+            $0.left.equalTo(nameEng.snp.left)
+        }
     }
 
 }
