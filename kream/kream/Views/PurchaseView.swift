21df803f4f6836535107e887f5327a7a71d7a2c9
alwn8918 <alwn8918@gmail.com>
2024-11-17 23:10:27 +0900

    💄 [week6] kream: 배송 버튼 구현

--- kream/kream/Views/PurchaseView.swift
+++ kream/kream/Views/PurchaseView.swift
@@ -59,6 +59,47 @@ class PurchaseView: UIView {
         $0.register(SizeCollectionViewCell.self, forCellWithReuseIdentifier: SizeCollectionViewCell.identifier)
     }
     
+    // 구분선
+    let topLine = UIView().then {
+        $0.backgroundColor = UIColor(red: 243/255, green: 243/255, blue: 243/255, alpha: 1)
+    }
+    
+    // 빠른배송 버튼
+    let quickButton = UIButton().then {
+        $0.backgroundColor = UIColor(red: 239/255, green: 98/255, blue: 84/255, alpha: 1)
+        $0.layer.cornerRadius = 10
+    }
+    
+    let quickPrice = UILabel().then {
+        $0.text = "345,000"
+        $0.font = .systemFont(ofSize: 13, weight: .semibold)
+        $0.textColor = .white
+    }
+    
+    let quickLabel = UILabel().then {
+        $0.text = "빠른배송(1~2일 소요)"
+        $0.font = .systemFont(ofSize: 10, weight: .regular)
+        $0.textColor = UIColor(red: 255/255, green: 202/255, blue: 185/255, alpha: 1)
+    }
+    
+    // 일반배송 버튼
+    let normalButton = UIButton().then {
+        $0.backgroundColor = UIColor(red: 34/255, green: 34/255, blue: 34/255, alpha: 1)
+        $0.layer.cornerRadius = 10
+    }
+    
+    let normalPrice = UILabel().then {
+        $0.text = "407,000"
+        $0.font = .systemFont(ofSize: 13, weight: .semibold)
+        $0.textColor = .white
+    }
+    
+    let normalLabel = UILabel().then {
+        $0.text = "일반배송(5~7일 소요)"
+        $0.font = .systemFont(ofSize: 10, weight: .regular)
+        $0.textColor = UIColor(red: 226/255, green: 226/255, blue: 226/255, alpha: 1)
+    }
+    
     // MARK: - init
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -80,7 +121,14 @@ class PurchaseView: UIView {
             imageView,
             nameEng,
             nameKor,
-            sizeCollectionView
+            sizeCollectionView,
+            topLine,
+            quickButton,
+            quickPrice,
+            quickLabel,
+            normalButton,
+            normalPrice,
+            normalLabel
         ].forEach {
             addSubview($0)
         }
@@ -92,7 +140,7 @@ class PurchaseView: UIView {
         
         subTitleLabel.snp.makeConstraints {
             $0.top.equalTo(titleLabel.snp.bottom)
-            $0.horizontalEdges.equalToSuperview().inset(158)
+            $0.horizontalEdges.equalToSuperview()
         }
         
         cancelButton.snp.makeConstraints {
@@ -121,6 +169,46 @@ class PurchaseView: UIView {
             $0.top.equalTo(imageView.snp.bottom).offset(35)
             $0.horizontalEdges.equalToSuperview().inset(15)
             $0.height.equalTo(104)
+        }
+        
+        topLine.snp.makeConstraints {
+            $0.horizontalEdges.equalToSuperview()
+            $0.bottom.equalToSuperview().offset(-95)
+            $0.height.equalTo(2)
+        }
+        
+        quickButton.snp.makeConstraints {
+            $0.top.equalTo(topLine.snp.bottom).offset(8)
+            $0.left.equalToSuperview().offset(18)
+            $0.width.equalTo(168)
+            $0.height.equalTo(52)
+        }
+        
+        quickPrice.snp.makeConstraints {
+            $0.top.equalTo(quickButton.snp.top).offset(11)
+            $0.centerX.equalTo(quickButton)
+        }
+        
+        quickLabel.snp.makeConstraints {
+            $0.bottom.equalTo(quickButton.snp.bottom).offset(-12)
+            $0.centerX.equalTo(quickButton)
+        }
+        
+        normalButton.snp.makeConstraints {
+            $0.top.equalTo(topLine.snp.bottom).offset(8)
+            $0.right.equalToSuperview().offset(-18)
+            $0.width.equalTo(168)
+            $0.height.equalTo(52)
+        }
+        
+        normalPrice.snp.makeConstraints {
+            $0.top.equalTo(normalButton.snp.top).offset(11)
+            $0.centerX.equalTo(normalButton)
+        }
+        
+        normalLabel.snp.makeConstraints {
+            $0.bottom.equalTo(normalButton.snp.bottom).offset(-12)
+            $0.centerX.equalTo(normalButton)
         }
     }
 
