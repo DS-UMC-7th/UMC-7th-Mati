d36f50814341a40c7c7d419c35df4069eb6d9273
alwn8918 <alwn8918@gmail.com>
2024-11-28 02:06:13 +0900

    🎉 [week8] 실습: 프로젝트 생성

---
+++ week8/week8practice/week8practice/SceneDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  SceneDelegate.swift
+//  week8practice
+//
+//  Created by 김미주 on 28/11/2024.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    var window: UIWindow?
+    
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene = windowScene
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+    
+}
+
