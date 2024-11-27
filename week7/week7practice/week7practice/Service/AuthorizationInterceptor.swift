2e6a8de342ee1e1fae359892b0ab4715ec31118d
alwn8918 <alwn8918@gmail.com>
2024-11-27 09:21:00 +0900

    ✨ [week7] 실습: API 연결

--- week7/week7practice/week7practice/Service/AuthorizationInterceptor.swift
+++ week7/week7practice/week7practice/Service/AuthorizationInterceptor.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+import Alamofire
+
+class AuthorizationInterceptor: RequestInterceptor {
+    private let kakaoKey: String
+    
+    init(kakaoKey: String) {
+        self.kakaoKey = kakaoKey
+    }
+    
+    func adapt(_ urlRequest: URLRequest, for session: Session, completion: @escaping (Result<URLRequest, any Error>) -> Void) {
+        var request = urlRequest
+        request.setValue("KakaoAK \(kakaoKey)", forHTTPHeaderField: "Authorization")
+        completion(.success(request))
+    }
+    
+    func retry(_ request: Request, for session: Session, dueTo error: any Error, completion: @escaping (RetryResult) -> Void) {
+        completion(.doNotRetry)
+    }
+}
