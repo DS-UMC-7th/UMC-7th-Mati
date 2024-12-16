d035315786e564154d442654ea8c4a49b02b3bf9
alwn8918 <alwn8918@gmail.com>
2024-12-17 00:24:39 +0900

    🎉 [week9] 실습: 프로젝트 생성

---
+++ week9/week9practice/week9practice/GestureImageView.swift
@@ -0,0 +1,21 @@
+//
+//  GestureImageView.swift
+//  week9practice
+//
+//  Created by 김미주 on 16/12/2024.
+//
+
+import UIKit
+
+class GestureImageView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = .white
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+
+}
---
+++ week9/week9practice/week9practice/ViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ViewController.swift
+//  week9practice
+//
+//  Created by 김미주 on 16/12/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
+
