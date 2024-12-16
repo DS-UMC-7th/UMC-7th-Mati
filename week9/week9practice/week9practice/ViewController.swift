416c9b8d90e6f7b136fb462a83ec48d52fd0283c
alwn8918 <alwn8918@gmail.com>
2024-12-17 00:59:11 +0900

    ✨ [week9] 실습: ViewController 작성

--- week9/week9practice/week9practice/ViewController.swift
+++ week9/week9practice/week9practice/ViewController.swift
@@ -8,6 +8,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view = gestureView
+    }
+    
+    private var gestureView = GestureImageView().then {
+        $0.loadImage(from: "https://i.namu.wiki/i/YIxNJW5sEG-sSI3DoSa5PBJNfm-UeWvBhL1BzA0dxRNEkZR-2pTr-BrseqswVmkbZ_n_GukxOOBTjx8Qct1E39lC5Am3BdrsuQcNhEwTnGLMRksjXfYqUNkH4QKRHc3Te5WjWkQrRZX3lPRjPQIUsg.webp")
+    }
 }
 
